d0a3795e7b988a809d7c18350dada1a849712453
ChabaJosa <chabagjg@gmail.com>
2020-05-07 19:55:48 -0400

    Already working to like tweets

--- twitter.py
+++ twitter.py
@@ -15,9 +15,24 @@ auth.set_access_token(key_3,key_4)
 
 api     = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user    = api.me()
+# me () is a tweepy thing: https://stackoverflow.com/questions/17386934/tweepy-api-me-not-returning-user-objects-of-friends
 
 # print(user) #Brings back user data
+# for follower in tweepy.Cursor(api.followers).items(): #Prints out all the users
+#     print(follower.name).encode("utf-8")
 
-for follower in tweepy.Cursor(api.followers).items(): #Prints out all the users
-    print(follower.name).encode("utf-8")
+# Do a big array of code keywords an randomize the output
+search      = "JavaScript"
+tweetQty    = 10
+
+for tweet in tweepy.Cursor(api.search, search).items(tweetQty):
+    try:
+        print("Tweet Liked")
+        tweet.favorite()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
 
