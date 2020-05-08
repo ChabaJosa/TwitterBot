80dbfd7b353b1fa30c389a3722d8f5e7ff36d57a
ChabaJosa <chabagjg@gmail.com>
2020-05-07 20:57:53 -0400

    Added multiple functions

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+import random
 from configparser import ConfigParser
 
 parser = ConfigParser()
@@ -9,30 +10,56 @@ key_2 = parser.get('TwitterCredentials','key_2')
 key_3 = parser.get('TwitterCredentials','key_3')
 key_4 = parser.get('TwitterCredentials','key_4')
 
+# http://docs.tweepy.org/en/latest/api.html?highlight=retweet#API.retweet
 auth    = tweepy.OAuthHandler(key_1,key_2)
 auth.set_access_token(key_3,key_4)
 
 
 api     = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user    = api.me()
-# me () is a tweepy thing: https://stackoverflow.com/questions/17386934/tweepy-api-me-not-returning-user-objects-of-friends
+# .me() is to use the authenticated user's profile
 
 # print(user) #Brings back user data
 # for follower in tweepy.Cursor(api.followers).items(): #Prints out all the users
 #     print(follower.name).encode("utf-8")
 
-# Do a big array of code keywords an randomize the output
-search      = "JavaScript"
-tweetQty    = 10
+topics   = ["JavaScript", "JS", "Python", "Py", "Pandas", "SciPy", "React", "NodeJS", "ExpressJS", "MongoDB", "MERN", "MERNSTACK", "Code", "Programming", "Bot", "ImAbot", "Developers", "Tech", "Django", "UdemyCode", "StackOverflow", "ReactNative", "SQL", "AI", "MachineLearning"]
+thyTimes = [10, 15, 8, 18, 9, 13, 25, 30, 20]
+# search   = random.choice(topics)
+tweetQty = len(topics)*random.choice(thyTimes)
 
-for tweet in tweepy.Cursor(api.search, search).items(tweetQty):
-    try:
-        print("Tweet Liked")
-        tweet.favorite()
-        time.sleep(10)
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
+# Try using RegEx here
+def Retweet():
+    search   = random.choice(topics)
+    for tweet in tweepy.Cursor(api.search, search).items(tweetQty):
+        try:
+            print("Tweet Retweeted with the keyword: ", search)
+            tweet.retweet()
+            time.sleep(random.choice(thyTimes))
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+def Favorite():
+    search   = random.choice(topics)
+    for tweet in tweepy.Cursor(api.search, search).items(tweetQty):
+        try:
+            print("Tweet Liked with the keyword: ", search)
+            tweet.favorite()
+            time.sleep(random.choice(thyTimes))
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+# def followTechie():
+#     for follower in tweepy.Cursor(api.followers).items():
+#     follower.follow()
+
+randomFunks = [Retweet(), Favorite() ]
+
+def Randomness():
+    random.choice(randomFunks)
 
 
